e93ad3e449af9bbeb125d7e8a79a0c1879e308ac
Felipe Gomes <felipe.gomes@aviorlogistica.com.br>
2022-11-16 16:51:25 -0300

    Added config to hide telescope preview window

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -7,7 +7,8 @@ require("telescope").setup({
     sorting_strategy = "ascending",
     scroll_strategy = "limit",
     layout_config = {
-      prompt_position = "top"
+      prompt_position = "top",
+      preview_cutoff = 9999999999999
     }
   }
 })
