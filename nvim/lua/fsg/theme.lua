467422101c8adf22469994279169e1f8bfee68f3
Felipe Gomes <felipe.gomes@aviorlogistica.com.br>
2023-03-28 09:44:06 -0300

    Minor changes

--- nvim/lua/fsg/theme.lua
+++ nvim/lua/fsg/theme.lua
@@ -27,7 +27,22 @@ require('kanagawa').setup({
 
     return {
       Whitespace = { fg = theme.ui.bg_p2 },
-      NonText = { fg = theme.ui.bg_p2 }
+      NonText = { fg = theme.ui.bg_p2 },
+
+      -- Telescope
+      TelescopeTitle = { fg = theme.ui.special, bold = true },
+      TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+      TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+      TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+      TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+      TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+      TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+      -- Popup menu
+      Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSbar = { bg = theme.ui.bg_m1 },
+      PmenuThumb = { bg = theme.ui.bg_p2 },
     }
   end,
   theme = "wave",
