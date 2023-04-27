f8ff33f3f70f8e6fc7a29d707f427e8d91ba4c2a
Felipe Gomes <felipe.gomes@aviorlogistica.com.br>
2023-04-27 14:49:22 -0300

    Removed friendly-snippets

--- nvim/lua/fsg/plugins/luasnip.lua
+++ nvim/lua/fsg/plugins/luasnip.lua
@@ -1,19 +1,6 @@
 return {
   "L3MON4D3/LuaSnip",
-  build = (not jit.os:find("Windows"))
-    and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
-    or nil,
-  dependencies = {
-    "rafamadriz/friendly-snippets",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-      require'luasnip'.filetype_extend("ruby", {"rails"})
-    end,
-  },
-  opts = {
-    history = true,
-    delete_check_events = "TextChanged",
-  },
+  build = "make install_jsregexp",
   -- stylua: ignore
   keys = {
     {
