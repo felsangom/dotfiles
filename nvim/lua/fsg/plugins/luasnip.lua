603824b5773c1a37482d638613eac5b9820e7cd7
Felipe Gomes <felipe.gomes@aviorlogistica.com.br>
2023-04-28 08:27:46 -0300

    Revert "Removed friendly-snippets"
    This reverts commit f8ff33f3f70f8e6fc7a29d707f427e8d91ba4c2a.

--- nvim/lua/fsg/plugins/luasnip.lua
+++ nvim/lua/fsg/plugins/luasnip.lua
@@ -1,6 +1,19 @@
 return {
   "L3MON4D3/LuaSnip",
-  build = "make install_jsregexp",
+  build = (not jit.os:find("Windows"))
+    and "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
+    or nil,
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require'luasnip'.filetype_extend("ruby", {"rails"})
+    end,
+  },
+  opts = {
+    history = true,
+    delete_check_events = "TextChanged",
+  },
   -- stylua: ignore
   keys = {
     {
