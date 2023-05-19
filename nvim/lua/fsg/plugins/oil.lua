5bab028cc98716a632baf8701116c51ac5740bcb
Felipe Gomes <felipe.gomes@aviorlogistica.com.br>
2023-05-19 16:05:49 -0300

    Minor oil config change

--- nvim/lua/fsg/plugins/oil.lua
+++ nvim/lua/fsg/plugins/oil.lua
@@ -2,6 +2,12 @@ return {
   'stevearc/oil.nvim',
   lazy = false,
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    default_file_explorer = true,
+    columns = {
+      "icon",
+    }
+  },
   config = function(_, opts)
     require('oil').setup(opts)
   end
