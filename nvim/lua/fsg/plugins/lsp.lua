7395358d7bf7abdbba92adac06695c11799ccc1b
Felipe Gomes <felipe.gomes@cooperalfa.coop.br>
2026-05-06 16:47:57 -0300

    Ajuste no pyright

--- nvim/lua/fsg/plugins/lsp.lua
+++ nvim/lua/fsg/plugins/lsp.lua
@@ -65,7 +65,7 @@ return {
                     },
                     python = {
                         analysis = {
-                            typeCheckingMode = "off",
+                            ignore = { '*' }
                         },
                     },
                 },
